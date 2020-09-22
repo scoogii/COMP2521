2cef159c891bf13420061d02d981ab8437652a07
Christian <christian.nguyen6@outlook.com>
2020-09-22 18:19:15 +1000

    Lab02 donzo

--- labs/lab02/benchmarkQueue.c
+++ labs/lab02/benchmarkQueue.c
@@ -5,14 +5,23 @@
 #include "Queue.h"
 
 int main(void) {
-	Queue q = QueueNew();
-	
-	// TODO
-	// Write a benchmark test to demonstrate the difference between
-	// ArrayQueue and CircularArrayQueue
-	
-	
-	
-	QueueFree(q);
-}
+    Queue q = QueueNew();
 
+    // Write a benchmark test to demonstrate the difference between
+    // ArrayQueue and CircularArrayQueue
+
+    // Aim:
+    // -> expand 3 times
+    // -> fill queue
+    // -> dequeue a couple times (has to loop to end)
+
+    // Enqueue a bunch of items
+    // Times for both programs shouldn't be too diff here
+    for (int i = 1; i < 100000; i++) { QueueEnqueue(q, i); }
+
+    // Dequeue a bunch of items
+    // Times should be VERY different here
+    for (int j = 1; j < 100000; j++) { QueueDequeue(q); }
+
+    QueueFree(q);
+}
