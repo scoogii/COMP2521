b27c8c187ee81c4a5b1d192113a154bcc0a02351
Christian <christian.nguyen6@outlook.com>
2020-09-22 16:41:03 +1000

    lab02 Task2

--- labs/lab02/ArrayQueue.c
+++ labs/lab02/ArrayQueue.c
@@ -6,12 +6,12 @@
 
 #include "Queue.h"
 
-#define DEFAULT_SIZE 16 // DO NOT change this line
+#define DEFAULT_SIZE 16  // DO NOT change this line
 
 struct queue {
-	Item *items;
-	int size;
-	int capacity;
+    Item *items;
+    int size;
+    int capacity;
 };
 
 static void increaseCapacity(Queue q);
@@ -20,52 +20,50 @@ static void increaseCapacity(Queue q);
  * Creates a new empty queue
  */
 Queue QueueNew(void) {
-	Queue q = malloc(sizeof(*q));
-	if (q == NULL) {
-		fprintf(stderr, "couldn't allocate Queue\n");
-		exit(EXIT_FAILURE);
-	}
-	
-	q->items = malloc(DEFAULT_SIZE * sizeof(Item));
-	if (q->items == NULL) {
-		fprintf(stderr, "couldn't allocate Queue\n");
-		exit(EXIT_FAILURE);
-	}
-	
-	q->size = 0;
-	q->capacity = DEFAULT_SIZE;
-	return q;
+    Queue q = malloc(sizeof(*q));
+    if (q == NULL) {
+        fprintf(stderr, "couldn't allocate Queue\n");
+        exit(EXIT_FAILURE);
+    }
+
+    q->items = malloc(DEFAULT_SIZE * sizeof(Item));
+    if (q->items == NULL) {
+        fprintf(stderr, "couldn't allocate Queue\n");
+        exit(EXIT_FAILURE);
+    }
+
+    q->size = 0;
+    q->capacity = DEFAULT_SIZE;
+    return q;
 }
 
 /**
  * Frees all resources associated with the given queue
  */
 void QueueFree(Queue q) {
-	free(q->items);
-	free(q);
+    free(q->items);
+    free(q);
 }
 
 /**
  * Adds an item to the end of the queue
  */
 void QueueEnqueue(Queue q, Item it) {
-	if (q->size == q->capacity) {
-		increaseCapacity(q);
-	}
-	
-	q->items[q->size++] = it;
+    if (q->size == q->capacity) { increaseCapacity(q); }
+
+    q->items[q->size++] = it;
 }
 
 /**
  * Doubles the capacity of the queue
  */
 static void increaseCapacity(Queue q) {
-	q->capacity *= 2;
-	q->items = realloc(q->items, q->capacity * sizeof(Item));
-	if (q->items == NULL) {
-		fprintf(stderr, "couldn't resize Queue\n");
-		exit(EXIT_FAILURE);
-	}
+    q->capacity *= 2;
+    q->items = realloc(q->items, q->capacity * sizeof(Item));
+    if (q->items == NULL) {
+        fprintf(stderr, "couldn't resize Queue\n");
+        exit(EXIT_FAILURE);
+    }
 }
 
 /**
@@ -73,14 +71,12 @@ static void increaseCapacity(Queue q) {
  * Assumes that the queue is not empty
  */
 Item QueueDequeue(Queue q) {
-	assert(q->size > 0);
-	
-	Item it = q->items[0];
-	for (int i = 1; i < q->size; i++) {
-		q->items[i - 1] = q->items[i];
-	}
-	q->size--;
-	return it;
+    assert(q->size > 0);
+
+    Item it = q->items[0];
+    for (int i = 1; i < q->size; i++) { q->items[i - 1] = q->items[i]; }
+    q->size--;
+    return it;
 }
 
 /**
@@ -88,31 +84,24 @@ Item QueueDequeue(Queue q) {
  * Assumes that the queue is not empty
  */
 Item QueueFront(Queue q) {
-	assert(q->size > 0);
-	
-	return q->items[0];
+    assert(q->size > 0);
+
+    return q->items[0];
 }
 
 /**
  * Gets the size of the given queue
  */
-int QueueSize(Queue q) {
-	return q->size;
-}
+int QueueSize(Queue q) { return q->size; }
 
 /**
  * Returns true if the queue is empty, and false otherwise
  */
-bool QueueIsEmpty(Queue q) {
-	return q->size == 0;
-}
+bool QueueIsEmpty(Queue q) { return q->size == 0; }
 
 /**
  * Prints the queue to the given file with items space-separated
  */
 void QueueDump(Queue q, FILE *fp) {
-	for (int i = 0; i < q->size; i++) {
-		fprintf(fp, "%d\n", q->items[i]);
-	}
+    for (int i = 0; i < q->size; i++) { fprintf(fp, "%d\n", q->items[i]); }
 }
-
