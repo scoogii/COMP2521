a43012668f10d2147a7c49656ea3b8a7624e82c5
Christian <christian.nguyen6@outlook.com>
2020-09-28 21:54:07 +1000

    fixing comments

--- labs/lab02/benchmarkQueue.c
+++ labs/lab02/benchmarkQueue.c
@@ -10,11 +10,6 @@ int main(void) {
     // Write a benchmark test to demonstrate the difference between
     // ArrayQueue and CircularArrayQueue
 
-    // Aim:
-    // -> expand 3 times
-    // -> fill queue
-    // -> dequeue a couple times (has to loop to end)
-
     // Enqueue a bunch of items
     // Times for both programs shouldn't be too diff here
     for (int i = 1; i < 100000; i++) { QueueEnqueue(q, i); }
