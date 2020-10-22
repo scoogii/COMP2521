c667640dd15c23338f58e2c68dc983defd9b37d9
Christian <christian.nguyen6@outlook.com>
2020-10-22 12:50:40 +1100

    Stage 1 assignment nearly done

---
+++ ass1/exmp1/invertedIndex.c
@@ -0,0 +1,213 @@
+////////////////////////////////////////////////////////////////////////
+// 					    COMP2521 Assignment 1                         //
+//					    Information Retrieval                         //
+//                       Implementation File                          //
+//                                                                    //
+//				     Christian Nguyen - z5310911                      //
+////////////////////////////////////////////////////////////////////////
+
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "invertedIndex.h"
+
+////////////////////////////////////////////////////////////////////////
+
+#define MAX_WORD_LEN 101
+
+////////////////////////////////////////////////////////////////////////
+
+FileList newFileListNode(char *filename, double tf);
+InvertedIndexBST InvertedIndexBSTNew(void);
+InvertedIndexBST FileListInsert(InvertedIndexBST tree, FileList fileNode);
+double calculateTf(char *filename, char *term);
+
+////////////////////////////////////////////////////////////////////////
+//                  ADAPTED BST FUNCTIONS FROM LAB03                  //
+////////////////////////////////////////////////////////////////////////
+
+
+/**
+ * Creates a new InvertedIndexBSTNode pointer and returns it
+ */
+InvertedIndexBST newInvertedIndexBST(char *key) {
+    InvertedIndexBST new = malloc(sizeof(struct InvertedIndexNode));
+
+    new->fileList = NULL;
+    new->left = NULL;
+    new->right = NULL;
+    new->word = strdup(key);
+
+    return new;
+}
+
+
+/**
+ * Creates a new FileListNode pointer and returns it
+ */
+FileList newFileListNode(char *filename, double tf) {
+    FileList new = malloc(sizeof(struct FileListNode));
+    new->filename = strdup(filename);
+    new->tf = tf;
+    new->next = NULL;
+
+    return new;
+}
+
+
+/**
+ * Inserts an initialised InvertedIndexBST into the current tree and
+ * returns the root of the updated tree. Does not insert duplicate keys
+ */
+InvertedIndexBST InvertedIndexBSTInsert(InvertedIndexBST tree, char *filename, char *key) {
+    if (tree == NULL) {
+        // Create a new tree node and insert word to filelist when tree node is NULL
+        tree = newInvertedIndexBST(key);
+        FileList fileListNodeNew = newFileListNode(filename, calculateTf(filename, key));
+        return FileListInsert(tree, fileListNodeNew);
+    }
+    else if (strcmp(key, tree->word) < 0) {
+        tree->left = InvertedIndexBSTInsert(tree->left, filename, key);
+    }
+    else if (strcmp(key, tree->word) > 0) {
+        tree->right = InvertedIndexBSTInsert(tree->right, filename, key);
+    }
+    else {
+        // If word is found, insert file into filelist for that tree node
+        FileList fileListNodeNew = newFileListNode(filename, calculateTf(filename, key));
+        return FileListInsert(tree, fileListNodeNew);
+    }
+
+    return tree;
+}
+
+
+/**
+ * Inserts a new FileListNode into the given InvertedIndexBST and returns
+ * the root of the updated InvertedIndexBST
+ */
+InvertedIndexBST FileListInsert(InvertedIndexBST tree, FileList fileNode) {
+    // Case 1. Tree is NULL
+    if (tree->fileList == NULL) {
+        tree->fileList = fileNode;
+        fileNode->next = NULL;
+        return tree;
+    }
+
+    // Case 2. If fileNode's filename is before first filename
+    if (strcmp(fileNode->filename, tree->fileList->filename) < 0) {
+        fileNode->next = tree->fileList;
+        tree->fileList = fileNode;
+        return tree;
+    }
+
+    // Case 3. fileNode to be inserted somewhere within the list
+    FileList prev = tree->fileList;
+    FileList curr = tree->fileList;
+    for (; curr != NULL; prev = curr, curr = curr->next) {
+        if (strcmp(fileNode->filename, curr->filename) < 0) {
+            prev->next = fileNode;
+            fileNode->next = curr;
+            return tree;
+        }
+    }
+
+    // Case 4. fileNode to be inserted as the last node in the list
+    prev->next = fileNode;
+    fileNode->next = NULL;
+    return tree;
+}
+
+
+////////////////////////////////////////////////////////////////////////
+//  					 PART 1 FUNCTIONS                             //
+////////////////////////////////////////////////////////////////////////
+
+
+/**
+ * Normalises a given string. See the spec for details. Note: you should
+ * modify the given string - do not create a copy of it.
+ */
+char *normaliseWord(char *str) {
+    // Make all characters lowercase
+    for (char *currChar = str; *currChar; currChar++) { *currChar = tolower(*currChar); }
+
+    // Remove leading whitespaces
+    while (isspace(str[0])) str++;
+
+    // Remove trailing whitespaces
+    char *lastChar = str + strlen(str) - 1;
+    while (isspace((*lastChar))) lastChar--;
+
+    // Remove ending punctuation mark
+    if (*lastChar == '.' || *lastChar == ',' || *lastChar == ';' || *lastChar == '?') lastChar--;
+
+    // Adding new null terminator taken from Stack Overflow
+    // Post was about how to trim trailing whitespaces
+    lastChar[1] = '\0';
+
+    return str;
+}
+
+
+/**
+ * Function takes in a file pointer and calculates the relative term
+ * frequency adjusted for the document length of file f
+ */
+double calculateTf(char *filename, char *term) {
+    int freq = 0;
+    int numWords = 0;
+    FILE *f = fopen(filename, "r");
+
+    char scannedTerm[MAX_WORD_LEN] = {};
+    while (fscanf(f, "%s", scannedTerm) != EOF) {
+        if (strcmp(normaliseWord(scannedTerm), term) == 0) freq++;
+        numWords++;
+    }
+
+    fclose(f);
+    return (freq / (double)numWords);
+}
+
+
+/**
+ * This function opens the collection file with the given name, and then
+ * generates an inverted index from those files listed in the collection
+ * file,  as  discussed  in  the spec. It returns the generated inverted
+ * index.
+ */
+InvertedIndexBST generateInvertedIndex(char *collectionFilename) {
+    // Create a new InvertedIndexBST that is currently empty
+    InvertedIndexBST root = NULL;
+
+    // Open collection file with error checking
+    FILE *collectionFile = fopen(collectionFilename, "r");
+    if (collectionFile == NULL) {
+        perror(collectionFilename);
+        exit(1);
+    }
+
+    char wordsFileName[MAX_WORD_LEN] = {};
+    while (fscanf(collectionFile, "%s", wordsFileName) != EOF) {
+        // Open the words file
+        FILE *wordsFile = fopen(wordsFileName, "r");
+        if (wordsFile == NULL) {
+            perror(wordsFileName);
+            exit(1);
+        }
+
+        // Parse words then insert/update the word's InvertedIndexBST node
+        char word[MAX_WORD_LEN] = {};
+        while (fscanf(wordsFile, "%s", word) != EOF) {
+            root = InvertedIndexBSTInsert(root, wordsFileName, normaliseWord(word));
+        }
+
+        fclose(wordsFile);
+    }
+
+    fclose(collectionFile);
+
+    return root;
+}
---
+++ ass1/exmp1/main.c
@@ -0,0 +1,51 @@
+////////////////////////////////////////////////////////////////////////
+// 					    COMP2521 Assignment 1                         //
+//					    Information Retrieval                         //
+//                     Main Program for Testing                       //
+//                                                                    //
+//				     Christian Nguyen - z5310911                      //
+////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+#include "invertedIndex.h"
+
+static void showBSTreeNode(InvertedIndexBST t);
+static void showListNodes(FileList list);
+void BSTreeInfix(InvertedIndexBST t);
+
+int main(void) {
+    InvertedIndexBST n = generateInvertedIndex("collection.txt");
+
+    // Check deets are right
+    BSTreeInfix(n);
+
+    return 0;
+}
+
+// Prints the value in the given node
+static void showBSTreeNode(InvertedIndexBST t) {
+    if (t == NULL) return;
+    printf("%s :=: ", t->word);
+}
+
+// Prints out the filelist
+static void showListNodes(FileList list) {
+    if (list == NULL) return;
+
+    while (list != NULL) {
+        printf("%s ( %lf ) -> ", list->filename, list->tf);
+        list = list->next;
+    }
+    printf("*\n");
+}
+
+// Prints the values in the given BSTree in infix order
+void BSTreeInfix(InvertedIndexBST t) {
+    if (t == NULL) return;
+
+    BSTreeInfix(t->left);
+    showBSTreeNode(t);
+    showListNodes(t->fileList);
+    BSTreeInfix(t->right);
+}
